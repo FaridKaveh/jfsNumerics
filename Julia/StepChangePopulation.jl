bc3b6d6d947a4fca80b8914f2c397599ff58acf2
Farid <faridkaveh20@imperial.ac.uk>
2024-08-16 16:19:48 +0100

    Added makeCorrelMat and makeProbMat

--- Julia/StepChangePopulation.jl
+++ Julia/StepChangePopulation.jl
@@ -27,40 +27,10 @@ function binSearch(x, ls)
 end
 
 
-function densityFlatPop(τ, c, s)
-    pushfirst!(s, 0)
-
-    n = length(s)
-
-    density = 1
-
-    for i in 2:n
-        density *= binomial(n-i+2,2)* exp(-binomial(n-i+2,2)*(s[i] - s[i-1]))
-    end 
+function densityStepChange(s::AbstractVector{Float64}, p)
     
-    return density 
-end
-
-function densityFlatPop2(τ, c, s)\
-    pushfirst!(s, 0)
-
-    n = length(s)
-
-    logSummands = [binomial(n-j+2,2) for j in 2:n]
-    logSummands = log.(logSummands)
-
-    intSummands = zeros(n-1)
-
-    for j in 2:n
-        intSummands[j-1] = (s[j]-s[j-1])
-        intSummands[j-1] *= binomial(n-j+2,2)
-    end
-
-    
-    return exp(sum(logSummands - intSummands))
-end
-function densityStepChange(τ, c, s)
-    
+    τ = p[1]
+    c = p[2]
     # τ is the step change point
     # c is the intensity after τ
     # s is the time vector
@@ -81,11 +51,17 @@ function densityStepChange(τ, c, s)
     end
 
     
-    return exp(sum(logSummands - intSummands))
+    return exp(sum(logSummands .- intSummands))
 end
 
+function branchLengthRatios(s::AbstractVector{Float64}, i, j) 
+    n= length(s)
+    s[n-i+1]*s[n-j+1]/(sum(s)+s[n])^2
+end 
+Integrand(u, p) = densityStepChange(u, p[1:2])*branchLengthRatios(u, p[3], p[4])
+
 #function for change of variables so that we can integrate on a hypercube
-function changeVar(func, τ, c, s)
+function changeVar(func, s, p)
     # τ is the step change point
     # c is the intensity after τ
     # s = (s_1, s_2, ..., s_{n-1}, t_n) is a vector such that t_i = t_n * ∏_{j=i}^{n-1} s_j
@@ -95,32 +71,36 @@ function changeVar(func, τ, c, s)
 
     jacobian = prod([s[j]^(j-1) for j in eachindex(s)])
 
-    return func(τ, c, times)*jacobian
+    return func(times, p)*jacobian
 end
 
 
-fStepChange(u, p) = changeVar(densityStepChange, p[1], p[2], u)
+fStepChange(u, p) = changeVar(densityStepChange, u, p)
 
-fConst(u,p) = changeVar(densityFlatPop, p[1], p[2], u)
 
 function fStepChange_InPlace(y, u, p)
-    y[1] = changeVar(densityFlatPop2, p[1], p[2], u) 
+    y[1] = changeVar(densityStepChange, u, p) 
 end 
 
-function solveIntOOP(func, n, τ, c, reltol)
+function fBranchLengthRatios(y, u::AbstractVector{Float64}, p)
+    ## The last two paramers of p are the indices i and j
+    y[1] = changeVar(Integrand, u, p) 
+end
+
+function solveIntOOP(func, n, p, reltol)
     #OOP is Out of Place
     lower_terminals = zeros(n)
     upper_terminals = push!(ones(n-1), Inf)
     domain = (lower_terminals, upper_terminals)
 
-    problem = IntegralProblem(func, domain, [τ, c])
+    problem = IntegralProblem(func, domain, p)
 
     sol= solve(problem, CubatureJLh(); reltol=reltol)
 
     return sol.u
 end
 
-function solveIntIP(func, n, τ, s, reltol)
+function solveIntIP(func, n, p, reltol)
 
     lower_terminals = zeros(n)
     upper_terminals = push!(ones(n-1), Inf)
@@ -128,11 +108,44 @@ function solveIntIP(func, n, τ, s, reltol)
 
     prototype = zeros(1)
 
-    problem = IntegralProblem(IntegralFunction(func, prototype), domain, [τ, s])
+    problem = IntegralProblem(IntegralFunction(func, prototype), domain, p)
 
     sol= solve(problem, CubatureJLh(); reltol=reltol)
 
     return sol.u
 end 
 
-    
+function makeCorrelMatIP(n, τ, c, reltol)
+    correlMat = zeros(n-1, n-1)
+    for i in 1:n-1
+        for j in 1:i
+            correlMat[i,j] = solveIntIP(fBranchLengthRatios, n-1, [τ, c, i, j], reltol)[1]
+            i != j ? correlMat[j,i] = correlMat[i,j] : nothing
+        end
+    end
+
+    return correlMat
+end
+
+function makeProbMat(n, τ, c, reltol)
+    correlMat = makeCorrelMatIP(n, τ, c, reltol)
+
+    probMat = zeros(n-1, n-1)
+
+    for i in 1:n-1
+        for j in 1:i
+
+            if i == n-1
+                probMat[i,j] = j == n-1 ? n^2 * correlMat[i,j] : n*(j+1)*(CorrelMat[i,j]-CorrelMat[i,j+1])
+            else 
+                probMat[i,j] = (i+1)*(j+1)*(CorrelMat[i,j]-CorrelMat[i+1,j]+CorrelMat[i+1,j+1]-CorrelMat[i+1,j])
+            end 
+            
+            i != j ? probMat[j,i] = probMat[i,j] : nothing
+        end 
+    end 
+
+    return probMat
+end
+
+ProbMat = makeProbMat(5, 1, 1/2 , 1e-6)
